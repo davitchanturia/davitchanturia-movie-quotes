7020626a0233128936afc3b5376f9e5459af8685
dato chanturia <datochanturia@redberry.ge>
2022-01-26 15:16:19 +0400

    fix: replace hardcoded url with named route

--- app/Http/Middleware/MustBeAdmin.php
+++ app/Http/Middleware/MustBeAdmin.php
@@ -7,18 +7,20 @@ use Illuminate\Http\Request;
 
 class MustBeAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        if(! auth()->user()?->email === 'datochanturia@redberry.ge'){
-            return redirect('/');
-        }
-        return $next($request);
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Closure                 $next
+	 *
+	 * @return mixed
+	 */
+	public function handle(Request $request, Closure $next)
+	{
+		if (!auth()->user()?->email === 'datochanturia@redberry.ge')
+		{
+			return redirect(route('homepage'));
+		}
+		return $next($request);
+	}
 }
